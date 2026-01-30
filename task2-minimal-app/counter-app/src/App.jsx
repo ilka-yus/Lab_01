51415cf4e18e29f4a197fc9d3c62c2b47254937d
ilka-yus <ilnara.u06@gmail.com>
2026-01-30 23:29:06 +0500

    edits

--- task2-minimal-app/counter-app/src/App.jsx
+++ task2-minimal-app/counter-app/src/App.jsx
@@ -1,24 +1,28 @@
 import { useState } from 'react';
+import './App.css';
 
 function Counter() {
+  //Используем useState для хранения значения
   const [count, setCount] = useState(0);
 
   const increment = () => setCount(prev => prev + 1);
   const decrement = () => setCount(prev => prev - 1);
 
   return (
-   <div style={{ padding: '20px', border: '1px solid #ccc' }}>
-    <h3>Counter: {count}</h3>
-    <button onClick={increment}>Increment</button>
-    <button onClick={decrement}>Decrement</button>
+   <div style={{ padding: '20px', border: '1px solid #ccc', borderRadius: '20px' }}>
+    <h3>Count: {count}</h3>
+    <div className='buttons'>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
    </div>
   );
 }
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lab 1.2: Minimal App</h1>
+    <div className="centered-container">
+      <h1>Counter App</h1>
       <Counter />
     </div>
   );
